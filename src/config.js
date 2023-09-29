f4b200ebcf3ef99e614d9fe655ac13498ae85ff7
karthigayani <karthi1826@gmail.com>
2023-09-29 18:16:11 +0530

    backend link updated

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
-    api: "https://url-shortener-backend-delta.vercel.app",
-    client: "https://url-shortener-frontend-xi.vercel.app"
+    api: "https://url-shortener-backend-0zd7.onrender.com",
+    client: "http://localhost:3000"
 }
 
 //for backend local    http://localhost:4000
-//for frontend local    http://localhost:4000
+//for frontend local    http://localhost:3000
