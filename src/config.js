306017163a4b527d7e5b3dddb2cfe4fee91c2336
karthigayani <karthi1826@gmail.com>
2023-09-29 18:41:36 +0530

    client updated

--- src/config.js
+++ src/config.js
@@ -1,6 +1,6 @@
 export const config = {
     api: "https://url-shortener-backend-0zd7.onrender.com",
-    client: "http://localhost:3000"
+    client: "https://url-shortener-frontend-m4ex.onrender.com"
 }
 
 //for backend local    http://localhost:4000
